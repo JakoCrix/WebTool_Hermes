af9f42da42e476448e5275a4bed271d247ee913d
JakoCrix <69356638+JakoCrix@users.noreply.github.com>
2021-05-20 00:22:03 +1000

    test and archive

--- test/NumComments_Plot.py
+++ test/NumComments_Plot.py
@@ -2,10 +2,8 @@
 import pandas as pd
 import os
 import sys
-sys.path.append(os.getcwd())
-
 import plotly.express as px
-
+from datetime import date
 
 # Data Extraction
 from Helper.Connections_Database import connect_to_odinprod
@@ -13,7 +11,13 @@ conn_odin_str, conn_odin_obj= connect_to_odinprod()
 SumComments_Raw = pd.read_sql_query("SELECT datetime_hour, title, sumcomments_hour FROM commentscount cc "
                                     "INNER JOIN subreddit_info si on cc.idsubreddit=si.idsubreddit", conn_odin_obj)
 
-fig = px.line(SumComments_Raw, x="datetime_hour", y="sumcomments_hour", color='title')
+# Filters
+subreddits_list = ["stocks", "wallstreetbets"]
+start_date = date(2021,1,1)
+end_date = date(2021,1,25)
 
+SumComments2= SumComments_Raw[SumComments_Raw["title"].isin(subreddits_list)]
 
+fig = px.line(SumComments2, x="datetime_hour", y="sumcomments_hour", color='title')
+fig.update_layout(xaxis_range=[start_date,end_date])
 fig.show()
